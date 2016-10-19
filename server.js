5d7fa1acee96a2044970f473e017d79564dceaf5
Vipin <vipin.kumar150@gmail.com>
2016-10-19 22:42:29 +0530

    Add other folders

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const Hapi = require('hapi');
 const config = require('./config');
 const routes = require('./routes');
-const Logger = require('./logger');
+const logger = require('./logger');
 
 const host = config('HOST');
 const port = config('PORT');
@@ -20,10 +20,9 @@ server.connection({
 server.route(routes);
 
 //start the server
-Logger.info('Starting server');
 server.start((error) => {
   if(error){
     throw error;
   }
-  Logger.info(`Server running at : ${server.info.uri}`);
+  logger.info(`Server running at : ${server.info.uri}`);
 });
